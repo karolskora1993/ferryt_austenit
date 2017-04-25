5763d7308428b6c42ba1310180c89820e44aa67d
karolskora1993 <karol.skora@wingu.de>
2017-04-25 11:09:21 +0200

    final v

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from math import exp
 from math import pow
 import matplotlib.pyplot as plt
 
-temp = 780
+temp = 820
 v_nagrz = 0.5
 Q = 140000
 R = 8.3144
@@ -12,38 +12,41 @@ D = d0 * exp(-Q/(R * temp_K)) * 1e10
 dx = 0.1
 dt = 0.001
 C_ALFA = (temp - 912)/-240
-last = 6
+print(C_ALFA)
+ksi = 0
 while (D*dt)/pow(dx, 2) > 0.5:
     dt -= 0.001
 
 c1 = []
-c2 = []
 
 for i in range(0, 100):
     if i < 6:
         c1.append(0.67)
-        c2.append(0)
+    elif i == 6:
+        c1.append(0.02)
+        ksi = i
     else:
         c1.append(0.02)
-        c2.append(0.02)
+
+    c2= c1[:]
 
 steps = 1000
 for i in range(0, steps):
-    for j in range(0, last+1):
+    for j in range(0, ksi+1):
         if j == 0:
             c2[j] = (1 - 2 * (D * dt / pow(dx, 2))) * c1[j] + (D * dt / pow(dx, 2)) * (c1[j] + c1[j + 1])
-        elif j == len(c2) - 1:
+        elif j == ksi:
             c2[j] = (1 - 2 * (D * dt / pow(dx, 2))) * c1[j] + (D * dt / pow(dx, 2)) * (c1[j - 1] + c1[j])
         else:
             c2[j] = (1 - 2 * (D * dt / pow(dx, 2))) * c1[j] + (D * dt / pow(dx, 2)) * (c1[j - 1] + c1[j + 1])
-    for k in range(0, last):
-        c1[k] = c2[k]
-    if c1[last] >= C_ALFA:
-        last += 1
-        print(last)
+    if ksi < len(c1) -1:
+        c2[ksi + 1] = c2[ksi]
+    c1 = c2[:]
+    if c1[ksi] >= C_ALFA and ksi<len(c1)-1:
+        ksi += 1
 
-for element in c1:
-    print(element)
+# for element in c1:
+#     print(element)
 
 x = []
 total_length = 0
