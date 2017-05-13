86a88dd1724eccc3ccabc014b29f66b8a872c37f
karolskora1993 <karol.skora@wingu.de>
2017-05-13 11:34:48 +0200

    +v_heat

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 from math import exp
 from math import pow
 import matplotlib.pyplot as plt
+import itertools
 
-temp = 820
-v_nagrz = 0.5
+temp = float(input("Podaj temperaturę: "))
+v_heat = float(input("Podaj temperaturę nagrzewania(V=0- stała temperatura): "))
+initial_temp = temp;
 Q = 140000
 R = 8.3144
-temp_K = temp + 273
 d0 = 0.000041
-D = d0 * exp(-Q/(R * temp_K)) * 1e10
 dx = 0.1
-dt = 0.001
-C_ALFA = (temp - 912)/-240
-print(C_ALFA)
-ksi = 0
-while (D*dt)/pow(dx, 2) > 0.5:
-    dt -= 0.001
+markers = itertools.cycle((',', '+', '^', '.', 'o', '*', 'h', 'X', 'D', '4', '8', '_'))
 
+
+def compute_temp(prev_temp, v_heat):
+    if v_heat == 0:
+        return prev_temp
+    else:
+        return prev_temp + dt * v_heat
+
+
+def plot_step(results):
+    x = []
+    total_length = 0
+    for k in range(0, len(results)):
+        x.append(total_length)
+        total_length += dx
+    plt.plot(x, c1, linestyle='solid', marker=next(markers))
+
+
+def compute_d(temp_c):
+    return d0 * exp(-Q/(R * (temp_c + 273))) * 1e10
+
+
+def compute_c_alfa(temp_c):
+    return abs((4.8513 - 0.005776 * temp_c))
+
+
+def neumann_cond(temp_c, initial_dt, dx):
+    dt = initial_dt;
+    while (compute_d(temp_c)*dt)/pow(dx, 2) > 0.5:
+        dt -= 0.001
+    return dt
+
+dt = neumann_cond(temp, 0.001, dx)
 c1 = []
+ksi = 0
 
 for i in range(0, 100):
     if i < 6:
@@ -28,31 +56,32 @@ for i in range(0, 100):
     else:
         c1.append(0.02)
 
-    c2= c1[:]
+c2 = c1[:]
+steps = 10000
+plot_steps = steps/5
 
-steps = 1000
+
 for i in range(0, steps):
     for j in range(0, ksi+1):
         if j == 0:
-            c2[j] = (1 - 2 * (D * dt / pow(dx, 2))) * c1[j] + (D * dt / pow(dx, 2)) * (c1[j] + c1[j + 1])
+            c2[j] = (1 - 2 * (compute_d(temp) * dt / pow(dx, 2))) * c1[j] + (compute_d(temp) * dt / pow(dx, 2)) * (c1[j] + c1[j + 1])
         elif j == ksi:
-            c2[j] = (1 - 2 * (D * dt / pow(dx, 2))) * c1[j] + (D * dt / pow(dx, 2)) * (c1[j - 1] + c1[j])
+            c2[j] = (1 - 2 * (compute_d(temp) * dt / pow(dx, 2))) * c1[j] + (compute_d(temp) * dt / pow(dx, 2)) * (c1[j - 1] + c1[j])
         else:
-            c2[j] = (1 - 2 * (D * dt / pow(dx, 2))) * c1[j] + (D * dt / pow(dx, 2)) * (c1[j - 1] + c1[j + 1])
-    if ksi < len(c1) -1:
+            c2[j] = (1 - 2 * (compute_d(temp) * dt / pow(dx, 2))) * c1[j] + (compute_d(temp) * dt / pow(dx, 2)) * (c1[j - 1] + c1[j + 1])
+    if ksi < len(c1) - 2:
         c2[ksi + 1] = c2[ksi]
-    c1 = c2[:]
-    if c1[ksi] >= C_ALFA and ksi<len(c1)-1:
+    if c2[ksi] >= compute_c_alfa(temp) and ksi < len(c2)-2:
         ksi += 1
+        print("ksi++")
+        c2[ksi] = 0.02
+    if i % plot_steps == 0:
+        plot_step(c1)
+    temp = compute_temp(temp, v_heat)
+    c1 = c2[:]
 
-# for element in c1:
-#     print(element)
-
-x = []
-total_length = 0
-for i in range(0, len(c1)):
-    x.append(total_length)
-    total_length += dx
-
-plt.plot(x, c1, 'ro')
+plt.title("Temperatura: {0} prędkość nagrzewania: {1}". format(initial_temp, v_heat))
+plt.xlabel("Odległość")
+plt.ylabel("Stężenie")
 plt.show()
+
